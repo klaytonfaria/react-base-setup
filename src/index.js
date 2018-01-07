3f85f4d558bc5aac79db4b485cedb7e81af419fe
Klayton Faria <klayton.souza@gmail.com>
2018-01-07 12:03:45 -0200

    feat(index): Add app index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import routes from './routes';
+import './shared/resources/icons-font.json';
+import './shared/styles/base';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        {routes}
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
